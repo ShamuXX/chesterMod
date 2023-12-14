765660b5595b2bcd3e0d8b78002ed6c5b678d2ae
Samuel Mendoza <samuel16mendoza@gmail.com>
2023-12-14 13:04:33 -0600

    recipe Added to create Eyebone Stick

--- src/main/java/com/docs/chestermod/init/InitItems.java
+++ src/main/java/com/docs/chestermod/init/InitItems.java
@@ -11,13 +11,14 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 
 public class InitItems {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(
-            ForgeRegistries.ITEMS, Chestermod.MODID);
+        public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(
+                        ForgeRegistries.ITEMS, Chestermod.MODID);
 
-    public static final RegistryObject<Item> EYEBONE = ITEMS.register(
-            "eye_bone", () -> new Item(new Item.Properties()
-                    .tab(CreativeModeTab.TAB_MISC)
-                    .rarity(Rarity.RARE)
-                    .fireResistant()));
+        public static final RegistryObject<Item> EYEBONE = ITEMS.register(
+                        "eye_bone", () -> new Item(new Item.Properties()
+                                        .tab(CreativeModeTab.TAB_MISC)
+                                        .rarity(Rarity.RARE)
+                                        .fireResistant()
+                                        .stacksTo(0)));
 
 }
